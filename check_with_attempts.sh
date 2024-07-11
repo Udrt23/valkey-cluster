bed9a3c0c12c2c070d87bb0d9600a0c1bac82880
Usrlich <119010850+Udrt23@users.noreply.github.com>
2024-07-11 20:06:28 +0300

    Скрипт для проверки процессов valkey-server

---
+++ check_with_attempts.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+ports=("6379" "6381")
+checkProcess() {
+    local port=$1
+    local processName="valkey-server"
+    local ipAddress=$(ip a | grep 'inet ' | awk '/ens18/ {print $2}' | cut -d'/' -f1)
+    local attempt=0
+    local logFile="/var/log/check_valkey.log"
+    while ((attempt < 3)); do
+        if pgrep -f "$processName $ipAddress:$port"; then
+            break
+        else
+            echo "$(date '+%Y-%m-%d %H:%M:%S') Процесс valkey-server на порту $port не найден. Перезапуск..." >>$logFile
+            if [[ $port == "6379" ]]; then
+                /opt/valkey/src/valkey-server /opt/valkey/valkey_6379.conf &
+            elif [[ $port == "6381" ]]; then
+                /opt/valkey/src/valkey-server /opt/valkey/valkey_6381.conf &
+            fi
+            let "attempt+=1"
+            sleep 1
+        fi
+    done
+    if [[ $attempt -eq 3 ]]; then
+        echo "$(date '+%Y-%m-%d %H:%M:%S') Ошибка запуска процесса valkey-server на порту $port после 3 попыток." >>$logFile
+    fi
+}
+
+while true; do
+    for port in "${ports[@]}"; do
+        checkProcess $port
+    done
+    sleep 1
+done
+
+# service
